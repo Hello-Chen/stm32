c8f869fbce1faa0d5173e2bc2dc0119a7d30948e
HelloChen <2421985222@qq.com>
2020-06-12 09:10:54 +0800

    init

---
+++ project_06-1_USART/cmake-build-debug/CMakeFiles/project_06-1_USART.elf.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_06-1_USART.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj"
+  "CMakeFiles/project_06-1_USART.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj"
+  "CMakeFiles/project_06-1_USART.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj"
+  "CMakeFiles/project_06-1_USART.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj"
+  "CMakeFiles/project_06-1_USART.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj"
+  "CMakeFiles/project_06-1_USART.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj"
+  "CMakeFiles/project_06-1_USART.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj"
+  "CMakeFiles/project_06-1_USART.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj"
+  "CMakeFiles/project_06-1_USART.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj"
+  "CMakeFiles/project_06-1_USART.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj"
+  "CMakeFiles/project_06-1_USART.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/project_06-1_USART.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
+  "CMakeFiles/project_06-1_USART.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  "CMakeFiles/project_06-1_USART.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c.obj"
+  "CMakeFiles/project_06-1_USART.elf.dir/Src/main.c.obj"
+  "CMakeFiles/project_06-1_USART.elf.dir/Src/stm32f1xx_hal_msp.c.obj"
+  "CMakeFiles/project_06-1_USART.elf.dir/Src/stm32f1xx_it.c.obj"
+  "CMakeFiles/project_06-1_USART.elf.dir/Src/syscalls.c.obj"
+  "CMakeFiles/project_06-1_USART.elf.dir/Src/sysmem.c.obj"
+  "CMakeFiles/project_06-1_USART.elf.dir/Src/system_stm32f1xx.c.obj"
+  "CMakeFiles/project_06-1_USART.elf.dir/Startup/startup_stm32f103vetx.s.obj"
+  "project_06-1_USART.elf"
+  "project_06-1_USART.elf.manifest"
+  "project_06-1_USART.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/project_06-1_USART.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

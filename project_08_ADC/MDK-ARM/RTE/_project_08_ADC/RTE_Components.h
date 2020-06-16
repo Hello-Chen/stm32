0e1d854b1e1ca1b2433d1f3c68eddd650d796512
HelloChen <2421985222@qq.com>
2020-06-16 23:15:59 +0800

    project_08_ADC

---
+++ project_08_ADC/MDK-ARM/RTE/_project_08_ADC/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'project_08_ADC' 
+ * Target:  'project_08_ADC' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
